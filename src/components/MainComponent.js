5f7460ce585df3f6fbbc14ac4025d22a9ad49cb7
Ana Cavadas <anacavadas91@gmail.com>
2018-09-01 17:57:15 +0100

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -20,7 +20,7 @@ class Main extends Component {
       dishes : DISHES,      
       promotions : PROMOTIONS,
       leaders : LEADERS,
-      comments :COMMENTS       
+      comments : COMMENTS       
     };
   }
   
@@ -34,12 +34,20 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return (
+      <DishDetail  dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route path="/contactus" component={Contact}/>          
           <Redirect to="/home"/>
         </Switch>
