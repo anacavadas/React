55fc1ed348a5689d2c80576bae5f08a25097c0e8
Ana Cavadas <anacavadas91@gmail.com>
2018-09-15 23:17:25 +0100

    Fetch from Server

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -8,6 +8,7 @@ export const Comments = (state = {
         case ActionTypes.ADD_COMMENTS:
             return {...state, isLoading: false, errMess: null, comments: action.payload};
         case ActionTypes.COMMENTS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, comments: []};
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
             comment.id = state.comments.length;
